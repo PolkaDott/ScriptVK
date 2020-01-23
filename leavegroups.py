e4449620fdec6c908d20d6efbe23fd754520eb15
unknown <andrey.gurt@yandex.ru>
2020-01-23 17:19:14 +0500

    final leaver

--- leavegroups.py
+++ leavegroups.py
@@ -1,33 +1,71 @@
 import vk
+import datetime
 import colorama
-from colorama import Fore, Style
+from colorama import Fore
 colorama.init()
-token = 'b698d21d19ad8723bf0fc3362bc71b9f5c9d4a16bbad9c682e2a249650a71665f523ddcb4f2e5e51406ae'
+print(Fore.LIGHTYELLOW_EX + 'Open this site:')
+print(Fore.BLUE + 'https://oauth.vk.com/authorize?client_id=7289997&scope=groups&response_type=token&v=5.103')
+print(Fore.LIGHTYELLOW_EX + 'Input your token: ', end='')
+token = input()
+#token = 'e433d3733242aede087d6b2947ca5fcf6d421be58300f4d22204aad142090813dea99ebee9045af3ab980'
 session = vk.Session(access_token=token)
 api = vk.API(session, v=5.103)
-user_id = api.users.get()[0].get('id')
+try:
+    user_id = api.users.get()[0].get('id')
+except vk.exceptions.VkAPIError:
+    print(Fore.RED + 'Authentication is failed')
+    exit(0)
 req = api.groups.get(user_id=user_id)
 ids = req.get('items')
 if len(ids) == 0:
     print(Fore.LIGHTRED_EX + "There're no groups")
     exit(0)
+file = open('groups-' + datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S") + '.txt', 'w', encoding='utf-8')
 groups = api.groups.getById(group_ids=ids)
 names = [x.get('name') for x in groups]
-print(Fore.LIGHTRED_EX + str(len(names)) + ' GROUPS:')
+print(Fore.LIGHTRED_EX + '\n' + str(len(names)) + ' GROUPS:')
 for i in range(len(names)):
     print(Fore.LIGHTBLUE_EX + f"{names[i]}: vk.com/club{ids[i]}")
-print(Fore.RED + "\nLEAVE THESE GROUPS???(Y/N)\n->", end='')
+    file.write(f"{names[i]}: vk.com/club{ids[i]}\n")
+file.close()
+print(Fore.RED + "\nLEAVE THESE GROUPS???(Y/N)" + Fore.RESET + "\n->", end='')
 inp = input()
 if inp != 'y' and inp != 'Y':
-    print('\nREJECTED')
+    print(Fore.RED + '\nREJECTED')
     exit(0)
+
+privates = []
 i = 0
 while i < len(ids):
     try:
-        if api.groups.leave(group_id=ids[i]) == 1:
+        if api.groups.getById(group_id=ids[i], fields='is_closed')[0].get('is_closed') != 0:
+            privates.append(ids[i])
+        elif api.groups.leave(group_id=ids[i]) == 1:
             print(Fore.GREEN + names[i] + ' vk.com/club' + str(ids[i]) + ' - Successfully!')
         else:
             print(Fore.RED + names[i] + ' vk.com/club' + str(ids[i]) + ' - Error!')
+    except vk.exceptions.VkAPIError as error:
+        i = i - 1
+    i = i + 1
+
+if len(privates) > 0:
+    print(Fore.LIGHTRED_EX + "\nThere're privates groups:")
+    for id in privates:
+        print(Fore.LIGHTBLUE_EX + f"{api.groups.getById(group_id=id)[0].get('name')}: vk.com/club{id}")
+    print(Fore.RED + "Leave their too(Y/N)?\n" + Fore.RESET + '->', end='')
+    inp = input()
+    if inp != 'y' and inp != 'Y':
+        exit(0)
+else:
+    exit(0)
+
+i = 0
+while i < len(privates):
+    try:
+        if api.groups.leave(group_id=privates[i]) == 1:
+            print(Fore.GREEN + names[i] + ' vk.com/club' + str(privates[i]) + ' - Successfully!')
+        else:
+            print(Fore.RED + names[i] + ' vk.com/club' + str(privates[i]) + ' - Error!')
     except vk.exceptions.VkAPIError as error:
         i = i - 1
     i = i + 1
