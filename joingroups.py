22f05922c0b11662b8e07b6ae961ee33d828325a
unknown <andrey.gurt@yandex.ru>
2020-01-23 19:24:47 +0500

    pair changes

--- joingroups.py
+++ joingroups.py
@@ -1,8 +1,20 @@
 import vk
 import time
-token = '7323df01f40cbccc7ae47c5761a27e5d94a4a442cccb6fafba6d5dc6d1ec2833e6ea296dca44b97c102dc'
+import colorama
+from colorama import Fore
+print(Fore.LIGHTYELLOW_EX + 'Open this site:')
+print(Fore.BLUE + 'https://oauth.vk.com/authorize?client_id=7289997&scope=groups&response_type=token&v=5.103')
+print(Fore.LIGHTYELLOW_EX + 'Input your token: ', end='')
+token = input()
 session = vk.Session(access_token=token)
 api = vk.API(session, v=5.103)
+try:
+    user_id = api.users.get()[0].get('id')
+except vk.exceptions.VkAPIError:
+    print(Fore.RED + 'Authentication is failed')
+    exit(0)
+print('Input name of file with ')
+filename = input()
 file = open('groups.txt', 'r')
 groups = file.read().split('\n')
 ids = [group[group.rfind('/')+5:] for group in groups]
@@ -24,7 +36,7 @@ while i < len(ids):
 if len(privates) > 0:
     for id in privates:
         print(f"{api.groups.getById(group_id=id)[0].get('name')}: {id}")
-    print("\n\n\nThere're private groups. Join them too?(Y/N)\n->", end='')
+    print("\n\n\nThere're private groups. Join them too?(Y/N)", end='')
     inp = input()
     if inp == 'y' or inp == 'Y':
         i = 0
@@ -46,7 +58,7 @@ names = [x.get('name') for x in groups]
 print(str(len(names))+'GROUPS:')
 for i in range(len(names)):
     print("{0}: vk.com/club{1}".format(names[i], ids[i]))
-print("\n\n\n\nLEAVE THESE GROUPS???(Y/N)\n->", end='')
+print("\n\n\n\nLEAVE THESE GROUPS???(Y/N)", end='')
 inp = input()
 if inp != 'y' and inp != 'Y':
     print('\nREJECTED')
