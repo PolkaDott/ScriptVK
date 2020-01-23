11a9288c8ab58a8841abe54942a5a944188b9163
PolkaDott <andrey.gurt@yandex.ru>
2020-01-23 13:38:48 +0500

    git commit -m

--- leavegroups.py
+++ leavegroups.py
@@ -2,38 +2,32 @@ import vk
 import colorama
 from colorama import Fore, Style
 colorama.init()
-token = '12074ed1388c45383ab0751cb5e70ff6978413ea80ded624cbbd7796b30228697bbe66a2c9c88e68d2303'
+token = 'b698d21d19ad8723bf0fc3362bc71b9f5c9d4a16bbad9c682e2a249650a71665f523ddcb4f2e5e51406ae'
 session = vk.Session(access_token=token)
 api = vk.API(session, v=5.103)
 user_id = api.users.get()[0].get('id')
 req = api.groups.get(user_id=user_id)
 ids = req.get('items')
+if len(ids) == 0:
+    print(Fore.LIGHTRED_EX + "There're no groups")
+    exit(0)
 groups = api.groups.getById(group_ids=ids)
 names = [x.get('name') for x in groups]
-print(Fore.CYAN + Style.BRIGHT + str(len(names)) + ' GROUPS:')
+print(Fore.LIGHTRED_EX + str(len(names)) + ' GROUPS:')
 for i in range(len(names)):
-    print(Fore.GREEN + f"{names[i]}: vk.com/club{ids[i]}")
-print(Fore.YELLOW + "\n\n\n\nLEAVE THESE GROUPS???(Y/N)\n->", end='')
+    print(Fore.LIGHTBLUE_EX + f"{names[i]}: vk.com/club{ids[i]}")
+print(Fore.RED + "\nLEAVE THESE GROUPS???(Y/N)\n->", end='')
 inp = input()
 if inp != 'y' and inp != 'Y':
     print('\nREJECTED')
     exit(0)
-isAccess = True
 i = 0
 while i < len(ids):
-	try:
-		api.groups.leave(group_id=ids[i])
-	except:
-		print('yeee')
-		i = i - 1
-	i = i + 1
-for id in ids:
     try:
-        if api.groups.leave(group_id=id) != 1:
-            isAccess = False
-    except vk.exceptions.VkAPIError:
-        print('yeee')
-print(isAccess)
-input()
-for id in ids:
-    api.groups.join(group_id=id)
+        if api.groups.leave(group_id=ids[i]) == 1:
+            print(Fore.GREEN + names[i] + ' vk.com/club' + str(ids[i]) + ' - Successfully!')
+        else:
+            print(Fore.RED + names[i] + ' vk.com/club' + str(ids[i]) + ' - Error!')
+    except vk.exceptions.VkAPIError as error:
+        i = i - 1
+    i = i + 1
